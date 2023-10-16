256faec23b222313c258f5de473a534f4dbc2c35
Geovane Siciliano <geobsiciliano@gmail.com>
2023-10-16 15:03:48 -0300

    Implementing the feature

---
+++ RandomChallenges/SortedSquaredArrayChallengeGeo.cs
@@ -0,0 +1,23 @@
+﻿namespace RandomChallenges
+{
+    public class SortedSquaredArrayChallengeGeo
+    {
+        public int[] SortedSquaredArray { get; set; }
+
+        public int[] SortedArray { get; set; }
+
+        public SortedSquaredArrayChallengeGeo(int[] sortedArray)
+        {
+            SortedSquaredArray = new int[sortedArray.Length];
+            SortedArray = sortedArray;
+        }
+
+        public void ComputeArray()
+        {
+            for (var i = 0; i < SortedArray.Length; i++)
+            {
+                SortedSquaredArray[i] = SortedArray[i] * SortedArray[i];
+            }
+        }
+    }
+}

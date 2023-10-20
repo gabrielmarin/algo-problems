d3b68d4c94f6386d2a6bf1ed86e2070ca4d895d4
Gabriel Marin <gabrielhenriquemarin@gmail.com>
2023-10-20 14:32:39 -0300

    challenge validate subsequence solved

---
+++ RandomChallenges/ValidateSubsequence/ValidateSubsequenceChallenge.cs
@@ -0,0 +1,18 @@
+﻿namespace RandomChallenges.ValidateSubsequence;
+
+public class ValidateSubsequenceChallenge
+{
+    // array = [5,1,22,25,6,-1,8,10]
+    // sequence = [1,6,-1,10]
+    // output = true
+    public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+    {
+        var sequenceIndex = 0; 
+        foreach (var item in array)
+        {
+            if(item == sequence[sequenceIndex]) sequenceIndex++;
+            if (sequenceIndex == sequence.Count) return true;
+        }
+        return sequenceIndex == sequence.Count;
+    }
+}

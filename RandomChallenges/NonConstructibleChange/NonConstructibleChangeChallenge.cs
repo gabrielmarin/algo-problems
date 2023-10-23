2f1884f3486bae3421922682efe67e9439240d7b
Gabriel Marin <gabrielhenriquemarin@gmail.com>
2023-10-23 20:24:53 -0300

    Non-constructible change implemented

---
+++ RandomChallenges/NonConstructibleChange/NonConstructibleChangeChallenge.cs
@@ -0,0 +1,22 @@
+﻿namespace RandomChallenges.NonConstructibleChange;
+
+public class NonConstructibleChangeChallenge
+{
+    //I struggled a freaking lot with this one and wasn't able to solved all of it till the end by myself
+    //I got right the sorting part and the currentChange variable idea, but not the "coin > currentChange + 1" rule
+    //time complexity O(n log n) due to the sorting step
+    //space complexity O(1), since there wasn't a need to create another data structure to manipulate the data
+    public static int NonConstructibleChange(int[] coins)
+    {
+        if(coins.Length == 0) return 1;
+        Array.Sort(coins);
+        var currentChange = 0;
+        foreach (var coin in coins)
+        {
+            if(coin > currentChange + 1) return currentChange + 1;
+            currentChange += coin;
+        }
+        
+        return ++currentChange;
+    }
+}
